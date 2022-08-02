cf4c991419210a31598ec4baf7c21625164f23ca
pstaal <peter.staal@gmail.com>
2022-08-02 17:43:48 +0200

    prevent password return from database

--- backend/models/user.js
+++ backend/models/user.js
@@ -20,7 +20,8 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    minlength: 8
+    minlength: 8,
+    select: false 
   },
   name: {
     type: String,
@@ -48,7 +49,7 @@ const userSchema = new mongoose.Schema({
 
 
 userSchema.statics.findUserByCredentials = function findUserByCredentials (email, password) {
-  return this.findOne({ email })
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Incorrect email or password'));
